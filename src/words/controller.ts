09242a28067b8f171894782718b33ebea4463659
Bartek Olma <bartek@ucreate.it>
2020-05-17 19:28:03 +0200

    Adds basic controller.

---
+++ src/words/controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { WordsService } from './service';
+import { CreateWordDto } from './create-word.dto';
+import { Word } from './word';
+
+@Controller('words')
+export class WordsController {
+  constructor(private readonly wordsService: WordsService) {}
+
+  @Get()
+  getWords(): Promise<Word[]> {
+    return this.wordsService.getWords();
+  }
+
+  @Post()
+  async addWords(@Body() createWordDTO: CreateWordDto) {
+    return await this.wordsService.addWord(createWordDTO);
+  }
+}
