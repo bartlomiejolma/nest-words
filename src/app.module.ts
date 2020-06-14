45b989adb41d054f1a22d981e696f88ced5d063a
Bartek Olma <bartek@ucreate.it>
2020-06-14 16:27:46 +0200

    Uses nest config module to import .env file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { WordsModule } from './words';
 
 @Module({
-  imports: [WordsModule],
+  imports: [WordsModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
