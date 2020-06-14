d114e567196aa0c0d35ca54b0d20ee501f37d8ab
Bartek Olma <bartek@ucreate.it>
2020-06-14 17:56:17 +0200

    Adds e2e test with adding new word.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -19,5 +19,12 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/words')
       .expect(200);
+  });
+
+  it('/words (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/words')
+      .send({ name: 'test' })
+      .expect(201);
   });
 });
