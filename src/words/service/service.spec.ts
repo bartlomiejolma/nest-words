c37e119962ed25ba43b3a9114dd1fc1250cbc2bd
Bartek Olma <bartek@ucreate.it>
2020-06-14 17:55:44 +0200

    Adds in memory repository for service test.

--- src/words/service/service.spec.ts
+++ src/words/service/service.spec.ts
@@ -1,22 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { WordsService } from '.';
 import { Word } from '../word';
 
+import { WORDS_REPOSITORY } from '../../consts';
+
+class InMemoryRepository {
+  private words: Word[];
+
+  constructor() {
+    this.words = [];
+  }
+  find() {
+    return this.words;
+  }
+  insert(word: Word) {
+    this.words.push(word);
+  }
+}
 describe('WordsService', () => {
+  let wordsService: WordsService;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        WordsService,
+        {
+          provide: WORDS_REPOSITORY,
+          useClass: InMemoryRepository,
+        },
+      ],
+    }).compile();
+
+    wordsService = await moduleRef.resolve(WordsService);
+  });
+
   describe('getWords', () => {
     it('should not throw', async () => {
-      const service = new WordsService();
-      await service.getWords();
+      await wordsService.getWords();
     });
   });
 
   describe('addWord', () => {
     it('should return newly added word', async () => {
       const word: Word = { name: 'foo' };
-      const service = new WordsService();
 
-      await service.addWord(word);
+      await wordsService.addWord(word);
 
-      const words = await service.getWords();
+      const words = await wordsService.getWords();
 
       expect(words[words.length - 1]).toBe(word);
     });
