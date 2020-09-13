0ea7666a76f45e6fea0b384143d8978d8f0c7b05
Bartek Olma <bartek@ucreate.it>
2020-09-13 20:53:27 +0200

    Adds validation for the name.

--- src/words/dtos/create-word.dto.ts
+++ src/words/dtos/create-word.dto.ts
@@ -1,7 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
 import { Word } from '../word';
 import { DefinitionDto } from './definition.dto';
 
 export class CreateWordDto implements Word {
+  @IsNotEmpty()
   name: string;
   definitions: DefinitionDto[];
 }
