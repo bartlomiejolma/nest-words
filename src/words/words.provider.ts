3c3ebd525b3b447dda3e683a327ec2be51539d22
Bartek Olma <bartek@ucreate.it>
2020-09-13 20:10:07 +0200

    Adds definitions repository to save definitions.

--- src/words/words.provider.ts
+++ src/words/words.provider.ts
@@ -1,13 +1,24 @@
-import { Connection, Repository } from 'typeorm';
+import { Connection } from 'typeorm';
 import { WordEntity } from './entities/words.entity';
 
-import { WORDS_REPOSITORY, DATABASE_CONNECTION } from 'src/consts';
+import {
+  WORDS_REPOSITORY,
+  DATABASE_CONNECTION,
+  DEFINITIONS_REPOSITORY,
+} from 'src/consts';
+import { DefinitionEntity } from './entities/definitions.entity';
 
 export const wordsProviders = [
   {
     provide: WORDS_REPOSITORY,
     useFactory: (connection: Connection) =>
       connection.getRepository(WordEntity),
+    inject: [DATABASE_CONNECTION],
+  },
+  {
+    provide: DEFINITIONS_REPOSITORY,
+    useFactory: (connection: Connection) =>
+      connection.getRepository(DefinitionEntity),
     inject: [DATABASE_CONNECTION],
   },
 ];
