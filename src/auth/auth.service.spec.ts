fb311f37e4cd43abcdd2541938176045dbadd17d
Bartek Olma <bartek@ucreate.it>
2020-10-13 21:42:56 +0200

    Imports module in the test.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -8,6 +9,7 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
+      imports: [UsersModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
