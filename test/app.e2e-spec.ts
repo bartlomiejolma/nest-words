05033bd7a5e04a4cd8ddf95d8833e0da40cd2276
Bartek Olma <bartek@ucreate.it>
2020-10-13 17:13:45 +0200

    Adds waterfall test.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { waterfall } from 'async';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -26,6 +27,28 @@ describe('AppController (e2e)', () => {
       .post('/words')
       .send({ name: 'test' })
       .expect(201);
+  });
+
+  it('/words (POST) with verify', () => {
+    const newWord = { name: 'test' };
+    return waterfall([
+      cb =>
+        request(app.getHttpServer())
+          .post('/words')
+          .send(newWord)
+          .expect(201, cb),
+      (results, cb) =>
+        request(app.getHttpServer())
+          .get('/words')
+          .expect(200, cb),
+      (results, cb) => {
+        const { body } = results;
+        console.log(body);
+        expect(body.length).toBeGreaterThan(0);
+        expect(body[body.length - 1]).toEqual(expect.objectContaining(newWord));
+        cb(null, results);
+      },
+    ]);
   });
 
   afterAll(async () => {
