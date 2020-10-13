a94966a02fb9153368e0336466e7fd2a498f6b2a
Bartek Olma <bartek@ucreate.it>
2020-10-13 17:57:41 +0200

    Uses joi to add validation schema.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 import { WordsModule } from './words';
 
 @Module({
-  imports: [WordsModule, ConfigModule.forRoot()],
+  imports: [
+    WordsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
