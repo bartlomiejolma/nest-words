bbf181c6739f9aae569c14cf10ee9d83c0694ce7
Bartek Olma <bartek@ucreate.it>
2020-10-13 21:29:50 +0200

    Exposes login route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
-  imports: [UsersModule],
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule],
+  controllers: [AuthController],
 })
 export class AuthModule {}
