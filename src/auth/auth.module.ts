afac63b6c600059e8f867ae2c1e68aa0f82e5232
Bartek Olma <bartek@ucreate.it>
2020-10-26 22:24:27 +0100

    Adds jwt module to auth.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
@@ -8,7 +10,18 @@ import { LocalStrategy } from './local.strategy';
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
