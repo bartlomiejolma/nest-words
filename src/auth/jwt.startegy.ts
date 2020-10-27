6c6647d08d5aee361eda3865d29ddec7770f3527
Bartek Olma <bartek@ucreate.it>
2020-10-27 13:20:55 +0100

    Uses jwt Strategy, not generic one.

--- src/auth/jwt.startegy.ts
+++ src/auth/jwt.startegy.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport';
-import { ExtractJwt } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
