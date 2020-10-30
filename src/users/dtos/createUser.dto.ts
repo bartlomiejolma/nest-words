95e6f31b12f87b5651697b98df6a8b30d04a266c
Bartek Olma <bartek@ucreate.it>
2020-10-30 21:31:12 +0100

    Removes the createUserDto.
    Fixes the query param to use property existing on the user.

--- src/users/dtos/createUser.dto.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../user.interface';
-
-export class CreateUserDto implements User {
-  email: string;
-  name: string;
-  password: string;
-}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,27 +2,25 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './user.entity';
-
-export type User = any;
+import { User } from './user.interface';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly usersRepository: Repository<User>,
+    private readonly usersRepository: Repository<UserEntity>,
   ) {}
 
-  async getByUsername(username: string) {
-    const user = await this.usersRepository.findOne({ where: { username } });
+  async getByUsername(name: string) {
+    const user = await this.usersRepository.findOne({ where: { name } });
     if (user) {
       return user;
     }
     throw new NotFoundException();
   }
 
-  async create(userData: CreateUserDto) {
+  async create(userData: User) {
     const newUser = await this.usersRepository.create(userData);
     await this.usersRepository.save(newUser);
     return newUser;
