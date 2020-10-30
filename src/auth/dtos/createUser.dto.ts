d9628d1709ded924a7808392b4fba3305e943afe
Bartek Olma <bartek@ucreate.it>
2020-10-30 22:12:00 +0100

    Adds validation for register user.

--- src/auth/dtos/createUser.dto.ts
+++ src/auth/dtos/createUser.dto.ts
@@ -1,7 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 import { User } from '../../users/user.interface';
 
 export class CreateUserDto implements User {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(7)
   password: string;
 }
