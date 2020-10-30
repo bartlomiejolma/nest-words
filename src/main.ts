eca8c76cbcd34a1df51450f30cd6c73cf77e8888
Bartek Olma <bartek@ucreate.it>
2020-10-30 22:34:44 +0100

    Exposes documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +10,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
