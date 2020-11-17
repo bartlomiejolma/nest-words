b8b45c9b663def49ddd14e5ff98f069ddd142f9e
Bartek Olma <bartek@ucreate.it>
2020-11-17 19:57:25 +0100

    Requires env variables conditionally.
    On heroku the DATABASE_URL is provided.
    Other variables are conditionally required.

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,22 @@ import { UsersModule } from './users/users.module';
     WordsModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_PORT: Joi.number().required(),
-        DATABASE_USER: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
+        DATABASE_URL: Joi.string(),
+        DATABASE_HOST: Joi.string(),
+        DATABASE_PORT: Joi.number(),
+        DATABASE_USER: Joi.string(),
+        DATABASE_PASSWORD: Joi.string(),
+        DATABASE_NAME: Joi.string(),
         PORT: Joi.number().default(3000),
         JWT_SECRET: Joi.string().required(),
-      }),
+      })
+        .or('DATABASE_URL', 'DATABASE_HOST')
+        .with('DATABASE_HOST', [
+          'DATABASE_PORT',
+          'DATABASE_USER',
+          'DATABASE_PASSWORD',
+          'DATABASE_NAME',
+        ]),
     }),
     DatabaseModule,
     AuthModule,
