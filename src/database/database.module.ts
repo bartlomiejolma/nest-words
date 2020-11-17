a01f5249991ee391ae224cae6e0c4a7342d9b5f2
Bartek Olma <bartek@ucreate.it>
2020-11-17 19:58:55 +0100

    Uses url to config ORM module.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
+        url: configService.get('DATABASE_URL'),
         host: configService.get('DATABASE_HOST'),
         port: configService.get('DATABASE_PORT'),
         username: configService.get('DATABASE_USER'),
