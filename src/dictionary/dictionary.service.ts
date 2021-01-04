736c0b4f29b012cc4ef378c92666e642df336aae
Bartek Olma <bartek@ucreate.it>
2021-01-04 19:37:34 +0100

    Implements parsing of the sample input

--- src/dictionary/dictionary.service.ts
+++ src/dictionary/dictionary.service.ts
@@ -1,7 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
+interface ApiPronunciations {
+  phoneticSpelling: string;
+}
+interface ApiExample {
+  text: string;
+}
+
+interface ApiSenses {
+  definitions: string[];
+  examples: ApiExample[];
+}
+interface ApiEntries {
+  pronunciations?: ApiPronunciations[];
+  senses: ApiSenses[];
+}
+
+interface ApiLexicalCategory {
+  text: string;
+}
+interface ApiLexicalEntries {
+  entries: ApiEntries[];
+  lexicalCategory: ApiLexicalCategory;
+}
+interface ApiSingleResult {
+  lexicalEntries: ApiLexicalEntries[];
+}
+interface ApiResponse {
+  id: string;
+  results: ApiSingleResult[];
+}
+
 export abstract class DictionaryApi {
-  abstract async getWord(word: string): Promise<Record<string, string>>;
+  abstract getWord(word: string): Promise<ApiResponse>;
 }
 @Injectable()
 export class DictionaryService {
@@ -12,7 +43,29 @@ export class DictionaryService {
     return this.parseApiResponse(apiResponse);
   }
 
-  parseApiResponse(apiResponse: any) {
-    return apiResponse;
+  parseApiResponse(apiResponse: ApiResponse) {
+    const name = apiResponse.id;
+    const phoneticSpelling =
+      apiResponse.results[0].lexicalEntries[0].entries[0].pronunciations[0]
+        .phoneticSpelling;
+    const lexicalCategory =
+      apiResponse.results[0].lexicalEntries[0].lexicalCategory.text;
+    const definitions = apiResponse.results.flatMap((result: ApiSingleResult) =>
+      result.lexicalEntries.flatMap((lexicalEntry: ApiLexicalEntries) =>
+        lexicalEntry.entries.flatMap(entry =>
+          entry.senses.flatMap(sense => sense.definitions),
+        ),
+      ),
+    );
+    const examples = apiResponse.results.flatMap((result: ApiSingleResult) =>
+      result.lexicalEntries.flatMap((lexicalEntry: ApiLexicalEntries) =>
+        lexicalEntry.entries.flatMap(entry =>
+          entry.senses.flatMap(sense =>
+            sense.examples.flatMap(example => example.text),
+          ),
+        ),
+      ),
+    );
+    return { name, phoneticSpelling, definitions, examples, lexicalCategory };
   }
 }
