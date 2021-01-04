23648eddc8692af0f90432723c1a88f08431f2c5
Bartek Olma <bartek@ucreate.it>
2021-01-04 21:34:55 +0100

    Adds new fields and examples to the word

--- src/words/word.ts
+++ src/words/word.ts
@@ -1,6 +1,10 @@
 import { Definition } from './definition';
+import { Example } from './example';
 
 export interface Word {
   name: string;
   definitions: Definition[];
+  phoneticSpelling?: string;
+  examples?: Example[];
+  lexicalCategory?: string;
 }
